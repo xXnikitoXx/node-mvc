f6af0eb90d7c005e1b800afec52985ca1911298f
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-07-16 20:11:02 +0300

    Updated renderer and added for loops.

--- utils/render.js
+++ utils/render.js
@@ -4,6 +4,14 @@ const { Iterator } = require("./iterator");
 class Renderer {
 	constructor(object) {
 		this.model = object;
+		this.model.users = [
+			{ name: "pesho", age: 12 },
+			{ name: "gosho", age: 13 },
+			{ name: "kolyo", age: 14 },
+		];
+		this.pattern = {
+			openTag: /<(if|for|switch)\b(.*)>/,
+		}
 	}
 
 	/**
@@ -18,18 +26,83 @@ class Renderer {
 			if (fs.existsSync(html))
 				html = fs.readFileSync(html).toString();
 		let items = new Iterator(this.model).ListItems();
-		if (html.includes("<if ")) {
-			html.match(/<if (.*)>([^]*?)<\/if>/gm)
-			.map(cs => ({
-				statement: () => cs.match(/<if ([^].*)>/)[1].replace("model", "this.model"),
-				body: () => cs.match(/<if .*>([^]*?)<\/if>/)[1],
-				html: () => cs,
-			}))
-			.map(cso => html = html.replace(cso.html(), eval(cso.statement()) ? cso.body() : ""));
+		while (html.match(this.pattern.openTag) != null) {
+			let match = html.match(this.pattern.openTag);
+			let index = match.index;
+			let [ tag, operator, statement ] = match;
+			let body = html.substring(index + tag.length);
+			let nextClosingTag = Renderer.Pair(operator, body);
+			body = body.substring(0, nextClosingTag);
+			switch (operator) {
+				case "if":
+					let result = eval(statement.replace("model.", "this.model."));
+					html = html.substring(0, index) + (result ? body : "") + html.substring(index + tag.length + nextClosingTag + `</${operator}>`.length);
+					break;
+				case "for":
+					let template = body;
+					body = "";
+
+					function iterateWith(str, values) {
+						let result = str;
+						for (let v in values) {
+							result = result.split("\n");
+							for (let l in result)
+								if (result[l].match(/<(if|for|switch|case)(.*)>/) != null)
+									result[l] = result[l].replace(new RegExp(`\\b(${v})\\b`, "g"), values[v]);
+							result = result.join("\n").replace(new RegExp(`{{${v}}}`, "g"), values[v]);
+						}
+						return result;
+					}
+
+					let variables = statement.split("let ")[1].split("=")[0].replace(/[\[\]]/g, "").replace(" ", "").split(",");
+					variables.toValues = function() {
+						let result = {};
+						let names = this;
+						let i = 0;
+						for (let arg of arguments)
+							result[names[i++]] = arg;
+						return result;
+					}
+					eval(`for (${statement.replace("model", "this.model")}) body += this.Render(iterateWith(template, variables.toValues(${variables.join(", ")})));`);
+					html = html.substring(0, index) + body + html.substring(index + tag.length + nextClosingTag + `</${operator}>`.length);
+					break;
+				case "switch":
+
+					break;
+			}
 		}
 		for (let item of items)
 			html = html.replace("{{model." + item.name + "}}", item.value);
 		return html;
+	}
+
+	static Pair(tag, body) {
+		let parts = body.split(`</${tag}>`);
+		let openTags = 0;
+		let closingTags = 0;
+		for (let p of parts) {
+			closingTags++;
+			let match = p.match(new RegExp(`<${tag}(.*)`, "g"));
+			if (match != null) 
+				openTags += match.length;
+			else {
+				if (closingTags > openTags)
+					break;
+			}
+		}
+		return Renderer.NthOccurrence(body, `</${tag}>`, closingTags);
+	}
+
+	static NthOccurrence(str, sub, n = 0) {
+		let times = 0;
+		let index = null;
+		if (n == 0)
+			return str.indexOf(sub);
+		while (times < n && index != -1) {
+			index = str.indexOf(sub, index + sub.length);
+			times++;
+		}
+		return index || str.indexOf(sub);
 	}
 }
 
