8ae75300989f0bf7be926dc4c76c390ab3b40610
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-08-16 09:12:38 +0300

    Created import controller for dynamic loading.

---
+++ utils/routes/import.js
@@ -0,0 +1,28 @@
+const { Renderer } = require("./../render");
+const { request } = require("express");
+
+/**
+ * The HTTP variant of importing HTML modules from templates.
+ * @function
+ * @param {Express.Application} app
+ * @param {any} utils
+ */
+module.exports = (app, utils) => {
+	utils.logger.messages.configuring("/import", "POST");
+	app.post("/import", (req, res, next) => {
+		utils.csrfProtection(req, res, () => {
+			if (utils.templates.isRedirect(req.body.template)) {
+				res.send("redirect " + req.body.template);
+			}
+			else {
+				req.body.template = `<import ${req.body.template}>\r\n\t<default>\r\n</import>\r\n<script>model = {{...model}};</script>`;
+				req.body.model.user = req.user;
+				req.body.model.error = "";
+				req.body.model.csrfToken = req.csrfToken();
+				let renderer = new Renderer(req.body.model, utils, true);
+				req.url = req.body.template;
+				res.send(renderer.Render(req.body.template));
+			}
+		})
+	});
+};
