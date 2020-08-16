41162a3d70fb06f3cf989b8875a4d162053758ee
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-08-16 09:10:23 +0300

    Updated middleware and security.

--- utils/middleware/middleware.js
+++ utils/middleware/middleware.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { ObjectId } = require("mongodb");
 const express = require("express");
+const helmet = require("helmet");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const bodyParser = require("body-parser");
@@ -16,7 +17,7 @@ let csrfProtection = csrf({ cookie: true });
  * @param {Express.Application} app
  * @returns {Object}
  */
-module.exports = function(app, utils) {
+module.exports = (app, utils) => {
 	utils.passport = passport;
 	utils.loginRedirect = loginRedirect;
 	utils.csrfProtection = csrfProtection;
@@ -24,6 +25,74 @@ module.exports = function(app, utils) {
 	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use(express.json({ type: [ "application/json", "text/plain"] }));
 	app.use(cookieParser());
+	app.use(helmet());
+	app.use((req, res, next) => {
+		let features = [
+			// "accelerometer 'none'",
+			// "ambient-light-sensor 'none'",
+			// "autoplay 'none'",
+			// "battery 'none'",
+			// "camera 'none'",
+			// "display-capture 'none'",
+			// "document-domain 'none'",
+			// "encrypted-media 'none'",
+			// "execution-while-not-rendered 'none'",
+			// "execution-while-out-of-viewport 'none'",
+			"fullscreen '*'",
+			// "geolocation 'none'",
+			// "gyroscope 'none'",
+			// "layout-animations 'none'",
+			// "legacy-image-formats 'none'",
+			// "magnetometer 'none'",
+			// "midi 'none'",
+			// "navigation-override 'none'",
+			// "oversized-images 'none'",
+			// "payment 'none'",
+			// "picture-in-picture 'none'",
+			// "publickey-credentials-get 'none'",
+			// "sync-xhr 'none'",
+			// "usb 'none'",
+			// "vr 'none'",
+			// "wake-lock 'none'",
+			// "screen-wake-lock 'none'",
+			// "web-share 'none'",
+			// "xr-spatial-tracking 'none'",
+		];
+		res.setHeader("Feature-Policy", features.join("; "));
+		let csp = {
+			"default": [
+				`'self'`,
+				`'unsafe-eval'`,
+				`'unsafe-inline'`,
+				`techlab.ddns.net`,
+				`*.google.com`,
+				`*.cloudflare.com`,
+				`*.facebook.com`,
+				`*.fontawesome.com`,
+			],
+			"img": [
+				`*`
+			],
+			"media": [
+				`*`
+			],
+			"script": [
+				`'self'`,
+				`'unsafe-eval'`,
+				`'unsafe-inline'`,
+				`techlab.ddns.net`,
+				`*.google.com`,
+				`*.cloudflare.com`,
+				`*.facebook.com`,
+				`*.fontawesome.com`,
+			],
+		};
+		res.setHeader("Content-Security-Policy", Object.keys(csp).map(src => `${src}-src ${csp[src].join(" ")}`).join("; "));
+		res.setHeader("Referrer-Policy", "strict-origin");
+		res.setHeader("Server", "Tech Lab");
+		res.setHeader("X-Powered-By", "Tech Lab");
+		next();
+	});
 	if (utils.db) {
 		app.use(session({
 			secret: process.env.SESSION_SECRET || "eksdi",
@@ -39,11 +108,7 @@ module.exports = function(app, utils) {
 		app.use(passport.initialize());
 		app.use(passport.session());
 	}
-	app.use(function (req, res, next) {
-		res.setHeader("X-Powered-By", "Tech Lab");
-		next();
-	});
-	
+
 	passport.serializeUser((user, done) => done(null, user._id));
 	passport.deserializeUser((id, done) =>
 		utils.db.Collection("users")
