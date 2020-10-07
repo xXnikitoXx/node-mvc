3575c5932e6143b3fb38870e8d30efcec796a14e
Nikolay Atanasov <xxnikitoxx@abv.bg>
2020-10-08 02:09:48 +0300

    Added for loop tag tests.

--- tests/renderer/for.js
+++ tests/renderer/for.js
@@ -12,8 +12,14 @@ module.exports = (() => {
 			it(message, () => {
 				let input = fs.readFileSync(`${__dirname}/${inputFile}`).toString();
 				let output = fs.readFileSync(`${__dirname}/${outputFile}`).toString();
-				let renderer = new Renderer({});
-				let renderedOutput = renderer.Render(input);
+				let renderer = new Renderer({
+					list: [
+						{ name: "George", age: 20, },
+						{ name: "Peter", age: 21, },
+						{ name: "Anna", age: 12, },
+					],
+				});
+				let renderedOutput = renderer.Render(input, { hostname: "localhost" }).replace(/\r\n/g, "\n").replace(/\n/g, "\r\n");
 				if (log)
 					console.log(renderedOutput);
 				expect(renderedOutput).to.equal(output);
@@ -22,5 +28,7 @@ module.exports = (() => {
 
 		test("renders the body of the tag X times", "for_1_input.html", "for_1_output.html");
 		test("works inside nested loops", "for_2_input.html", "for_2_output.html");
+		test("works with object properties", "for_3_input.html", "for_3_output.html");
+		test("works with spread syntax", "for_4_input.html", "for_4_output.html");
 	});
 })();
